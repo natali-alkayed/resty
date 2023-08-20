4e38f9ab7b47802d9e89d313b1c024617165a55c
natali-alkayed <nataly2000alkayed@gmail.com>
2023-08-20 11:10:43 +0000

    lab28 done

--- resty/src/app.jsx
+++ resty/src/app.jsx
@@ -1,54 +1,44 @@
-import React, { useState } from 'react';
-import './app.scss';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Form from './components/form/index';
+import Results from './components/results/index'; 
 import Header from './components/header/index';
 import Footer from './components/footer/index';
-import Form from './components/form/index';
-import Results from './components/results/index';
+import './app.scss';
 
 function App() {
-  const [request, setRequest] = useState({ method: 'get', url: '', body: '' });
+  const [request, setRequest] = useState(null);
   const [response, setResponse] = useState(null);
-  const [loading, setLoading] = useState(false);
 
-  const handleFormSubmit = async () => {
-    setLoading(true);
-    try {
-      const requestOptions = {
-        method: request.method.toUpperCase(),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-       // body: request.body,
-      };
-
-      if (['post', 'put'].includes(request.method)) {
-        requestOptions.body = JSON.stringify(JSON.parse(request.body));
-      }
-
-      const response = await fetch(request.url, requestOptions);
-      const responseData = await response.json();
-      console.log('API Response:', responseData); 
-      setResponse(responseData);
-    } catch (error) {
-      console.error('Error:', error);
-    } finally {
-      setLoading(false);
+  useEffect(() => {
+    if (request) {
+      axios({
+        method: request.method,
+        url: request.url,
+        data: request.body,
+      })
+        .then((res) => {
+          setResponse(res.data);
+        })
+        .catch((error) => {
+          setResponse({ error: error.message });
+        });
     }
+  }, [request]);
+
+  const handleFormSubmit = (requestData) => {
+    setRequest(requestData);
+    setResponse(null); 
   };
 
   return (
-    <React.Fragment>
-      <Header />
-      <Form
-        request={request}
-        setRequest={setRequest}
-        handleFormSubmit={handleFormSubmit}
-        loading={loading}
-      />
-      <Results loading={loading} response={response} />
-      <Footer />
-    </React.Fragment>
+    <div>
+      <Header/>
+      <Form onSubmit={handleFormSubmit} />
+      {response && <Results data={response} />}
+      <Footer/>
+    </div>
   );
-};
+}
 
 export default App;
